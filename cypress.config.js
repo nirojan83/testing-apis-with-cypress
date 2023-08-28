eed5797bb163829a32c744953082e567cddb6d21
Nirojan Yogeswaran <niro83@hotmail.com>
2023-08-28 12:38:08 +0100

    First commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://aqueous-brook-60480.herokuapp.com/",
+    specPattern: "cypress/integration/api-tests/*.spec.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
